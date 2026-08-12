41ab6c825ef43541acc9dedebf24c912f6be1247
nandaniwmit <nandani.wmit@gmail.com>
2026-08-12 15:57:59 +0530

    Update usePageTracker.ts

--- src/hooks/usePageTracker.ts
+++ src/hooks/usePageTracker.ts
@@ -6,7 +6,7 @@ export function usePageTracker() {
   const prevPathnameRef = useRef<string>(location.pathname);
 
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
